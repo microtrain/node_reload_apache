136de1d666ee921159e6095382e74e7bab336088
Jason Snider <root@jasonsnider.com>
2020-02-14 16:09:43 -0600

    Proof of concept

---
+++ re.js
@@ -0,0 +1,15 @@
+const { exec } = require("child_process");
+
+//Disable the target site config(s)
+exec(`sudo a2dissite * && sudo systemctl reload apache2 && sudo a2ensite * && systemctl reload apache2`, {cwd: '/etc/apache2/sites-available'}, (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+
+    console.log(stdout);
+});
